bc4d25f7d60110902fc14e3573a935a7826e11f5
j-helland <jonathan.w.helland@gmail.com>
2024-08-26 17:43:13 -0600

    scene interface

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const AllocatorError = std.mem.Allocator.Error;
 
 const math = @import("math.zig");
 const Vec3 = math.Vec3;
@@ -7,20 +8,76 @@ const Color = Vec3;
 const vec3s = math.vec3s;
 
 pub const HitRecord = struct {
+    const Self = @This();
+
     point: Point3 = .{0, 0, 0},
     normal: Vec3 = .{0, 0, 0},
-    t: f32 = 0.0,
+    t: f32 = std.math.inf(f32),
+    b_front_face: bool = false,
+
+    pub fn setFrontFaceNormal(self: *Self, ray: *const Ray, outward_normal: Vec3) void {
+        self.b_front_face = (math.dot(ray.direction, outward_normal) < 0.0);
+        self.normal = 
+            if (self.b_front_face) outward_normal 
+            else -outward_normal;
+    } 
+};
+
+pub const HitContext = struct {
+    ray: *const Ray,
+    tmin: f32,
+    tmax: f32,
 };
 
 pub const Entity = union(enum) {
     const Self = @This();
 
     sphere: SphereEntity,
+    collection: EntityCollection,
 
-    pub fn hit(self: Self, ray: *const Ray, tmin: f32, tmax: f32, hit_record: *HitRecord) bool {
+    pub fn hit(self: Self, ctx: HitContext, hit_record: *HitRecord) bool {
         return switch (self) {
-            .sphere => |e| e.hit(ray, tmin, tmax, hit_record),
+            .sphere => |e| e.hit(ctx, hit_record),
+            .collection => |e| e.hit(ctx, hit_record)
         };
+    }
+};
+
+pub const EntityCollection = struct {
+    const Self = @This();
+
+    entities: std.ArrayList(Entity),
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{ .entities = std.ArrayList(Entity).init(allocator) };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.entities.deinit();
+    }
+
+    pub fn add(self: *Self, entity: Entity) AllocatorError!void {
+        try self.entities.append(entity);
+    }
+
+    pub fn hit(self: *const Self, _ctx: HitContext, hit_record: *HitRecord) bool {
+        var ctx = _ctx;
+        var hit_record_tmp  = HitRecord{};
+        var b_hit_anything = false;
+        var closest_t = ctx.tmax;
+
+        for (self.entities.items) |*entity| {
+            if (entity.hit(ctx, &hit_record_tmp)) {
+                b_hit_anything = true;
+                closest_t = hit_record_tmp.t;
+                ctx.tmax = closest_t;
+
+                // We know this hit is closest because we already reduced the search range bound tmax.
+                hit_record.* = hit_record_tmp;
+            }
+        }
+
+        return b_hit_anything;
     }
 };
 
@@ -30,12 +87,12 @@ pub const SphereEntity = struct {
     center: Point3,
     radius: f32,
 
-    pub fn hit(self: *const Self, ray: *const Ray, tmin: f32, tmax: f32, hit_record: *HitRecord) bool {
+    pub fn hit(self: *const Self, ctx: HitContext, hit_record: *HitRecord) bool {
         // direction from ray to sphere center
-        const oc = self.center - ray.origin;
+        const oc = self.center - ctx.ray.origin;
         // Detect polynomial roots for ray / sphere intersection equation (cx-x)^2 + (cy-y)^2 + (cz-z)^2 = r^2 = (c - p(t)) . (c - p(t))
-        const a = math.dot(ray.direction, ray.direction);
-        const h = math.dot(ray.direction, oc);
+        const a = math.dot(ctx.ray.direction, ctx.ray.direction);
+        const h = math.dot(ctx.ray.direction, oc);
         const c = math.dot(oc, oc) - self.radius * self.radius;
         const discriminant = h*h - a*c;
 
@@ -43,16 +100,17 @@ pub const SphereEntity = struct {
 
         const disc_sqrt = @sqrt(discriminant);
         var root = (h - disc_sqrt) / a;
-        if ((root <= tmin) or (root >= tmax)) {
+        if ((root <= ctx.tmin) or (ctx.tmax <= root)) {
             root = (h + disc_sqrt) / a;
-            if ((root <= tmin) or (root >= tmax)) {
+            if ((root <= ctx.tmin) or (ctx.tmax <= root)) {
                 return false;
             }
         }
 
         hit_record.t = root;
-        hit_record.point = ray.at(hit_record.t);
-        hit_record.normal = (hit_record.point - self.center) / vec3s(self.radius);
+        hit_record.point = ctx.ray.at(hit_record.t);
+        const outward_normal = (hit_record.point - self.center) / vec3s(self.radius);
+        hit_record.setFrontFaceNormal(ctx.ray, outward_normal);
 
         return true;
     }
@@ -77,33 +135,20 @@ fn writeColor(writer: *const std.fs.File.Writer, color: *const Vec3) !void {
     try writer.print("{} {} {}\n", .{ir, ig, ib}); 
 }
 
-fn rayColor(ray: *const Ray) Vec3 {
-    const sphere = Entity{ .sphere = SphereEntity{ .center = .{0, 0, -1}, .radius = 0.5 } };
-
-    var hit_record = HitRecord{};
-    if (sphere.hit(ray, 0.0, 1.0, &hit_record)) {
-        return vec3s(0.5) * (hit_record.normal + vec3s(1.0));
+fn rayColor(ray: *const Ray, entity: *const Entity) Vec3 {
+    const ctx = HitContext{ 
+        .ray = ray, 
+        .tmin = 0.0, 
+        .tmax = std.math.inf(f32),
+    };
+    var record = HitRecord{};
+    if (entity.hit(ctx, &record)) {
+        return vec3s(0.5) * (record.normal + vec3s(1.0));
     }
 
     const d = math.normalize(ray.direction);
     const a = 0.5 * (d[1] + 1.0);
     return vec3s(1.0 - a) * Color{1, 1, 1} + vec3s(a) * Color{0.5, 0.7, 1.0}; 
-}
-
-fn sphereHit(ray: *const Ray, center: Point3, radius: f32) f32 {
-    // direction from ray to sphere center
-    const oc = center - ray.origin;
-    // Detect polynomial roots for ray / sphere intersection equation (cx-x)^2 + (cy-y)^2 + (cz-z)^2 = r^2 = (c - p(t)) . (c - p(t))
-    const a = math.dot(ray.direction, ray.direction);
-    const h = math.dot(ray.direction, oc);
-    const c = math.dot(oc, oc) - radius * radius;
-    const discriminant = h*h - a*c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (h - @sqrt(discriminant)) / a;
-    }
 }
 
 pub fn main() !void {
@@ -132,6 +177,21 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("P3\n{} {}\n255\n", .{img_width, img_height});
 
+    // ---- allocator ----
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // ---- scene initialization ----
+    var scene = EntityCollection.init(allocator);
+    defer scene.deinit();
+
+    try scene.add(Entity{ .sphere = SphereEntity{ .center = Point3{0, 0, -1}, .radius = 0.5 } });
+    try scene.add(Entity{ .sphere = SphereEntity{ .center = Point3{0, -100.5, -1}, .radius = 100.0 } });
+
+    const world = Entity{ .collection = scene };
+
+    // ---- rendering pass ----
     for (0..img_height) |v| {
         std.log.info("Scanlines remaining: {}", .{img_height - v});
 
@@ -139,7 +199,7 @@ pub fn main() !void {
             const pixel_center = pixel00_loc + pixel_delta_u * vec3s(@floatFromInt(u)) + pixel_delta_v * vec3s(@floatFromInt(v));
             const ray_direction = pixel_center - camera_center;
             const ray = Ray{ .origin = camera_center, .direction = ray_direction };
-            const color = rayColor(&ray);
+            const color = rayColor(&ray, &world);
 
             try writeColor(&stdout, &color);
         }
--- src/math.zig
+++ src/math.zig
@@ -9,25 +9,6 @@ pub inline fn vec3s(x: f32) Vec3 {
 
 const Vec3Component = enum(i32) { x, y, z };
 
-pub inline fn swizzle(
-    v: Vec3, 
-    comptime x: Vec3Component, 
-    comptime y: Vec3Component,
-    comptime z: Vec3Component
-) Vec3 {
-    return @shuffle(f32, v, undefined, [3]i32{ @intFromEnum(x), @intFromEnum(y), @intFromEnum(z) });
-}
-
-test "swizzle" {
-    const v = Vec3{ 1, 2, 3 };
-
-    const v1 = swizzle(v, .x, .y, .z);
-    try std.testing.expectEqual(v, v1);
-
-    const v2 = swizzle(v, .z, .y, .x);
-    try std.testing.expectEqual(Vec3{3, 2, 1}, v2);
-}
-
 pub inline fn dot(u: Vec3, v: Vec3) f32 {
     const xmm = u * v;
     return xmm[0] + xmm[1] + xmm[2];
@@ -50,7 +31,7 @@ test "length" {
 }
 
 pub inline fn normalize(u: Vec3) Vec3 {
-    return u / @as(Vec3, @splat(length(u)));
+    return u / vec3s(length(u));
 }
 
 test "normalize" {
